0e6199cce84cafe8c9ce79bf7b7d7cc49a1c5051
erhuodidi <114044979+erhuodidi@users.noreply.github.com>
2022-09-21 16:29:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: o4wK85yY1Zg7bUOBnsvnBIltu7IM",
+      id:o4wK852YH91xh6J2huezUZKCPBUg",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "Y6vXMksP5BC2_fQQc9-iCW7kYnF9oHNAM9EmESnYFIY",
       // 所在省份
